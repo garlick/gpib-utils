a7eae5135790680dfba62bac89076d417d91afcf
garlick <garlick@5738911d-5f10-0410-ae8c-d51ad623c20f>
2007-11-11 04:26:01 +0000

    fully working ics8065 utility

--- trunk/src/ics8065.c
+++ trunk/src/ics8065.c
@@ -26,7 +26,7 @@
 #include "errstr.h"
 #include "ics.h"
 
-#define OPTIONS "n:resgiIj:cfCtT:mM:sS:zZ:kK:"
+#define OPTIONS "n:reiIj:cfCtT:mM:sS:zZ:kK:gG:vV:qQ:wW:"
 static struct option longopts[] = {
         {"name",                required_argument,  0, 'n'},
         {"get-interface-name",  no_argument,        0, 'I'},
@@ -39,14 +39,22 @@ static struct option longopts[] = {
         {"set-ip-number",       required_argument,  0, 'Z'},
         {"get-netmask",         no_argument,        0, 'k'},
         {"set-netmask",         required_argument,  0, 'K'},
-        {"get-ren-mode",        no_argument,        0, 'm'},
+        {"get-gateway",         no_argument,        0, 'g'},
+        {"set-gateway",         required_argument,  0, 'G'},
+        {"get-keepalive",       no_argument,        0, 'v'},
+        {"set-keepalive",       required_argument,  0, 'V'},
+        {"get-gpib-address",    no_argument,        0, 'q'},
+        {"set-gpib-address",    required_argument,  0, 'Q'},
+        {"get-system-controller",no_argument,       0, 'w'},
+        {"set-system-controller",required_argument, 0, 'W'},
         {"set-ren-mode",        required_argument,  0, 'M'},
+        {"get-ren-mode",        no_argument,        0, 'm'},
         {"reload-config",       no_argument,        0, 'C'},
         {"reload-factory",      no_argument,        0, 'f'},
         {"commit-config",       no_argument,        0, 'c'},
         {"reboot",              no_argument,        0, 'r'},
         {"get-idn-string",      no_argument,        0, 'i'},
-        {"errlog",              no_argument,        0, 'e'},
+        {"get-errlog",          no_argument,        0, 'e'},
         {0, 0, 0, 0}
 };
 
@@ -69,6 +77,14 @@ usage(void)
   "  -Z,--set-ip-number        set IP number (a.b.c.d)\n"
   "  -k,--get-netmask          get netmask (a.b.c.d)\n"
   "  -K,--set-netmask          set netmask (a.b.c.d)\n"
+  "  -g,--get-gateway          get gateway (a.b.c.d)\n"
+  "  -G,--set-gateway          set gateway (a.b.c.d)\n"
+  "  -v,--get-keepalive        get keepalive time (in seconds)\n"
+  "  -V,--set-keepalive        set keepalive time (in seconds)\n"
+  "  -q,--get-gpib-address     get gpib address\n"
+  "  -Q,--set-gpib-address     set gpib address\n"
+"  -w,--get-system-controller get sys controller mode (0=disabled, 1=enabled)\n"
+"  -W,--set-system-controller set sys controller mode (0=disabled, 1=enabled)\n"
   "  -m,--get-ren-mode         get REN active at boot (0=false, 1=true)\n"
   "  -M,--set-ren-mode         set REN active at boot (0=false, 1=true)\n"
   "  -C,--reload-config        force reload of default config\n"
@@ -98,6 +114,14 @@ main(int argc, char *argv[])
     char *set_ip_number = NULL;
     int get_netmask = 0;
     char *set_netmask = NULL;
+    int get_gateway = 0;
+    char *set_gateway = NULL;
+    int get_keepalive = 0;
+    int set_keepalive = -1;
+    int get_gpib_address = 0;
+    int set_gpib_address = -1;
+    int get_system_controller = 0;
+    int set_system_controller = -1;
     int get_ren_mode = 0;
     int set_ren_mode = -1;
     int reload_config = 0;
@@ -139,10 +163,34 @@ main(int argc, char *argv[])
                 set_ip_number = optarg;
                 break;
             case 'k' :  /* --get-netmask */
-                get_ip_number = 1;
+                get_netmask = 1;
                 break;
             case 'K' :  /* --set-netmask */
                 set_netmask = optarg;
+                break;
+            case 'g' :  /* --get-gateway */
+                get_gateway = 1;
+                break;
+            case 'G' :  /* --set-gateway */
+                set_gateway = optarg;
+                break;
+            case 'v' :  /* --get-keepalive */
+                get_keepalive = 1;
+                break;
+            case 'V' :  /* --set-keepalive */
+                set_keepalive = strtoul(optarg, NULL, 0);
+                break;
+            case 'q' :  /* --get-gpib-address */
+                get_gpib_address = 1;
+                break;
+            case 'Q' :  /* --set-gpib-address */
+                set_gpib_address = strtoul(optarg, NULL, 0);
+                break;
+            case 'w' :  /* --get-system-controller */
+                get_system_controller = 1;
+                break;
+            case 'W' :  /* --set-system-controller */
+                set_system_controller = strtoul(optarg, NULL, 0);
                 break;
             case 'm' :  /* --get-ren-mode */
                 get_ren_mode = 1;
@@ -182,6 +230,10 @@ main(int argc, char *argv[])
             && !get_static_ip_mode && set_static_ip_mode == -1
             && !get_ip_number && !set_ip_number
             && !get_netmask && !set_netmask
+            && !get_gateway && !set_gateway
+            && !get_keepalive && set_keepalive == -1
+            && !get_system_controller && set_system_controller == -1
+            && !get_gpib_address && set_gpib_address == -1
             && !get_ren_mode && set_ren_mode == -1
             && !get_comm_timeout && set_comm_timeout == -1)
         usage();
@@ -190,13 +242,17 @@ main(int argc, char *argv[])
     if (!ics)
         exit(1);
 
+    /* idn_string */
     if (get_idn) {
         char *tmpstr;
 
         if (ics_idn_string(ics, &tmpstr) != 0)
             goto done;
         fprintf(stderr, "%s: %s\n", prog, tmpstr);
+        free(tmpstr);
     }
+
+    /* interface_name */
     if (get_interface_name) {
         char *tmpstr;
 
@@ -209,6 +265,8 @@ main(int argc, char *argv[])
         if (ics_set_interface_name(ics, set_interface_name) != 0)
             goto done;
     }
+
+    /* comm_timeout */
     if (get_comm_timeout) {
         unsigned int timeout;
 
@@ -220,6 +278,8 @@ main(int argc, char *argv[])
         if (ics_set_comm_timeout(ics, set_comm_timeout) != 0)
             goto done;
     }
+
+    /* static_ip_mode */
     if (get_static_ip_mode) {
         int flag;
 
@@ -231,6 +291,8 @@ main(int argc, char *argv[])
         if (ics_set_static_ip_mode(ics, set_static_ip_mode) != 0)
             goto done;
     }
+    
+    /* ip_number */
     if (get_ip_number) {
         char *tmpstr;
 
@@ -243,6 +305,8 @@ main(int argc, char *argv[])
         if (ics_set_ip_number(ics, set_ip_number) != 0)
             goto done;
     }
+
+    /* netmask */
     if (get_netmask) {
         char *tmpstr;
 
@@ -255,6 +319,61 @@ main(int argc, char *argv[])
         if (ics_set_netmask(ics, set_netmask) != 0)
             goto done;
     }
+
+    /* gateway */
+    if (get_gateway) {
+        char *tmpstr;
+
+        if (ics_get_gateway(ics, &tmpstr) != 0)
+            goto done;
+        fprintf(stderr, "%s: %s\n", prog, tmpstr);
+        free(tmpstr);
+    }
+    if (set_gateway) {
+        if (ics_set_gateway(ics, set_gateway) != 0)
+            goto done;
+    }
+
+    /* keepalive */
+    if (get_keepalive) {
+        unsigned int t;
+
+        if (ics_get_keepalive(ics, &t) != 0)
+            goto done;
+        fprintf(stderr, "%s: %u\n", prog, t);
+    }
+    if (set_keepalive != -1) {
+        if (ics_set_keepalive(ics, set_keepalive) != 0)
+            goto done;
+    }
+
+    /* gpib_address */
+    if (get_gpib_address) {
+        unsigned int a;
+
+        if (ics_get_gpib_address(ics, &a) != 0)
+            goto done;
+        fprintf(stderr, "%s: %u\n", prog, a);
+    }
+    if (set_gpib_address != -1) {
+        if (ics_set_gpib_address(ics, set_gpib_address) != 0)
+            goto done;
+    }
+
+    /* system_controller */
+    if (get_system_controller) {
+        int flag;
+
+        if (ics_get_system_controller(ics, &flag) != 0)
+            goto done;
+        fprintf(stderr, "%s: %u\n", prog, flag);
+    }
+    if (set_system_controller != -1) {
+        if (ics_set_system_controller(ics, set_system_controller) != 0)
+            goto done;
+    }
+
+    /* ren_mode */
     if (get_ren_mode) {
         int flag;
 
@@ -266,21 +385,29 @@ main(int argc, char *argv[])
         if (ics_set_ren_mode(ics, set_ren_mode) != 0)
             goto done;
     }
+
+    /* reload_factory */
     if (reload_factory) {
         if (ics_reload_factory(ics) != 0)
             goto done;
         fprintf(stderr, "%s: factory config reloaded\n", prog);
     }
+
+    /* reload_config */
     if (reload_config) {
         if (ics_reload_config(ics) != 0)
             goto done;
         fprintf(stderr, "%s: default config reloaded\n", prog);
     }
+
+    /* commit_config */
     if (commit_config) {
         if (ics_commit_config(ics) != 0)
             goto done;
         fprintf(stderr, "%s: current config written\n", prog);
     }
+
+    /* get_errlog */
     if (get_errlog) {
         unsigned int *tmperr;
         int i, tmpcount = 0;
@@ -292,6 +419,7 @@ main(int argc, char *argv[])
         free(tmperr);
     }
 
+    /* reboot */
     if (reboot) {
         if (ics_reboot(ics) != 0)
             goto done;
