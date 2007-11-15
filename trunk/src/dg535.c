b39f24e88692c36bb19220b02a1297f17a3058b6
garlick <garlick@5738911d-5f10-0410-ae8c-d51ad623c20f>
2007-11-15 02:44:47 +0000

    new device initialization routine complete dg535 utility

--- trunk/src/dg535.c
+++ trunk/src/dg535.c
@@ -44,15 +44,18 @@ static strtab_t out_names[] = DG535_OUT_NAMES;
 static strtab_t out_modes[] = DG535_OUT_MODES;
 static strtab_t trig_modes[] = DG535_TRIG_MODES;
 
-#define OPTIONS "n:clvo:qQ:aA:fF:pP:yY:tT:mM:sS:bB:zZ:D:x"
+#define OPTIONS "n:clve:o:dD:qQ:aA:fF:pP:yY:tT:mM:sS:bB:zZ:D:x"
 static struct option longopts[] = {
     {"name",            required_argument, 0, 'n'},
     {"clear",           no_argument,       0, 'c'},
     {"local",           no_argument,       0, 'l'},
     {"verbose",         no_argument,       0, 'v'},
-    {"display-string",  required_argument, 0, 'D'},
+    {"display-string",  required_argument, 0, 'e'},
     {"single-shot",     no_argument,       0, 'x'},
     {"out-chan",        required_argument, 0, 'o'},
+
+    {"get-delay",       no_argument,       0, 'd'},
+    {"set-delay",       required_argument, 0, 'D'},
 
     {"get-out-mode",    no_argument,       0, 'q'},
     {"set-out-mode",    required_argument, 0, 'Q'},
@@ -97,9 +100,11 @@ usage(void)
 "  -c,--clear                  initialize instrument to default values\n"
 "  -l,--local                  return instrument to local operation on exit\n"
 "  -v,--verbose                show protocol on stderr\n"
-"  -D,--display-string         display string (1-20 chars), empty to clear\n"
+"  -e,--display-string         display string (1-20 chars), empty to clear\n"
 "  -x,--single-shot            trigger instrument in single shot mode\n"
 "  -o,--out-chan               select output channel (t0|a|b|ab|c|d|cd)\n"
+"  -d,--get-delay              get output delay\n"
+"  -D,--set-delay              set output delay (chan,secs)\n"
 "  -q,--get-out-mode           get output mode\n"
 "  -Q,--set-out-mode           set output mode (ttl|nim|ecl|var)\n"
 "  -a,--get-out-ampl           get output amplitude\n"
@@ -120,8 +125,6 @@ usage(void)
 "  -B,--set-burst-count        set trigger burst count (2:19)\n"
 "  -z,--get-trig-z             get trigger input impedence\n"
 "  -Z,--set-trig-z             set trigger input impedence (hi|lo)\n"
-
-"  -d,--delay                  output delay (chan,secs)\n"
            , prog, addr ? addr : "no default");
     exit(1);
 }
@@ -189,6 +192,23 @@ _interpret_status(gd_t gd, unsigned char status, char *msg)
 }
 
 int
+parse_delay(char *str, int *chanp, double *delayp)
+{
+    char *cstr, *dstr;
+    int c;
+
+    if (!(cstr = strtok(str, ",")))
+        return 0;
+    if (!(dstr = strtok(NULL, ",")))
+        return 0;
+    if ((c = rfindstr(out_names, cstr)) == -1)
+        return 0;
+    *chanp = c;
+    *delayp = strtod(dstr, NULL);
+    return 1;
+}
+
+int
 main(int argc, char *argv[])
 {
     char *addr = NULL;
@@ -198,6 +218,9 @@ main(int argc, char *argv[])
     gd_t gd;
     char *display_string = NULL;
     int out_chan = -1;
+    int get_delay = 0;
+    double set_delay_time = HUGE_VALF;
+    int set_delay_chan = -1;
     int get_out_mode = 0;
     int set_out_mode = -1;
     int get_out_ampl = 0;
@@ -242,7 +265,7 @@ main(int argc, char *argv[])
             local = 1;
             todo++;
             break;
-        case 'D': /* --display-string */
+        case 'e': /* --display-string */
             display_string = optarg;
             todo++;
             break;
@@ -253,9 +276,20 @@ main(int argc, char *argv[])
         case 'o': /* --out-chan */
             out_chan = rfindstr(out_names, optarg);
             if (out_chan == -1) {
-                fprintf(stderr, "%s: bad --out-chan optarg\n", prog);
+                fprintf(stderr, "%s: bad --out-chan argument\n", prog);
                 exit(1);
             }
+            break;
+        case 'd': /* --get-delay */
+            get_delay = 1;
+            todo++;
+            break;
+        case 'D': /* --set-delay */
+            if (!parse_delay(optarg, &set_delay_chan, &set_delay_time)) {
+                fprintf(stderr, "%s: bad --set-delay argument\n", prog);
+                exit(1);
+            }
+            todo++;
             break;
         case 'q' : /* --get-out-mode */
             get_out_mode = 1;
@@ -405,6 +439,10 @@ main(int argc, char *argv[])
         fprintf(stderr, "%s: --get/set-out-z needs --out-chan\n", prog);
         exit(1);
     }
+    if (out_chan == -1 && (get_delay || set_delay_chan != -1)) {
+        fprintf(stderr, "%s: --get/set-delay needs --out-chan\n", prog);
+        exit(1);
+    }
 
     gd = gpib_init(addr, _interpret_status, 0);
     if (!gd) {
@@ -428,27 +466,85 @@ main(int argc, char *argv[])
     if (set_out_mode != -1)
         gpib_wrtf(gd, "%s %d,%d", DG535_OUT_MODE, out_chan, set_out_mode);
     if (get_out_mode) {
-        /* FIXME */
+        int i;
+
+        gpib_wrtf(gd, "%s %d", DG535_OUT_MODE, out_chan);
+        if (gpib_rdf(gd, "%d", &i) != 1) {
+            fprintf(stderr, "%s: error reading output mode\n", prog);
+            exit_val = 1;
+            goto done;
+        }
+        fprintf(stderr, "%s: %s\n", prog, findstr(out_modes, i));
     }
     if (set_out_ampl != HUGE_VALF)
         gpib_wrtf(gd, "%s %d,%lf", DG535_OUT_AMPL, out_chan, set_out_ampl);
     if (get_out_ampl) {
-        /* FIXME */
+        double d;
+
+        gpib_wrtf(gd, "%s %d", DG535_OUT_AMPL, out_chan);
+        if (gpib_rdf(gd, "%lf", &d) != 1) {
+            fprintf(stderr, "%s: error reading output amplitude\n", prog);
+            exit_val = 1;
+            goto done;
+        }
+        fprintf(stderr, "%s: %lf\n", prog, d);
     }
     if (set_out_offset != HUGE_VALF)
         gpib_wrtf(gd, "%s %d,%lf", DG535_OUT_OFFSET, out_chan, set_out_offset);
     if (get_out_offset) {
-        /* FIXME */
+        double d;
+
+        gpib_wrtf(gd, "%s %d", DG535_OUT_OFFSET, out_chan);
+        if (gpib_rdf(gd, "%lf", &d) != 1) {
+            fprintf(stderr, "%s: error reading output offset\n", prog);
+            exit_val = 1;
+            goto done;
+        }
+        fprintf(stderr, "%s: %lf\n", prog, d);
     }
     if (set_out_polarity != -1)
         gpib_wrtf(gd, "%s %d,%d", DG535_OUT_POLARITY, out_chan, set_out_polarity);
     if (get_out_polarity) {
-        /* FIXME */
+        int i;
+
+        gpib_wrtf(gd, "%s %d", DG535_OUT_POLARITY, out_chan);
+        if (gpib_rdf(gd, "%d", &i) != 1) {
+            fprintf(stderr, "%s: error reading output polarity\n", prog);
+            exit_val = 1;
+            goto done;
+        }
+        fprintf(stderr, "%s: %s\n", prog, i == 0 ? "-" : "+");
     }
     if (set_out_z != -1)
         gpib_wrtf(gd, "%s %d,%d", DG535_TERM_Z, out_chan, set_out_z);
     if (get_out_z) {
-        /* FIXME */
+        int i;
+
+        gpib_wrtf(gd, "%s %d", DG535_TERM_Z, out_chan);
+        if (gpib_rdf(gd, "%d", &i) != 1) {
+            fprintf(stderr, "%s: error reading output impedance\n", prog);
+            exit_val = 1;
+            goto done;
+        }
+        fprintf(stderr, "%s: %s\n", prog, i == 0 ? "lo" : "hi");
+    }
+
+    /* delay */
+    if (set_delay_chan != -1) {
+        gpib_wrtf(gd, "%s %d,%d,%lf", DG535_DELAY_TIME, out_chan,
+                set_delay_chan, set_delay_time);
+    }
+    if (get_delay) {
+        int i;
+        double d;
+
+        gpib_wrtf(gd, "%s %d", DG535_DELAY_TIME, out_chan);
+        if (gpib_rdf(gd, "%d,%lf", &i, &d) != 2) {
+            fprintf(stderr, "%s: error reading delay time\n", prog);
+            exit_val = 1;
+            goto done;
+        }
+        fprintf(stderr, "%s: %s,%lf\n", prog, findstr(out_names, i), d);
     }
 
     /* trigger */
