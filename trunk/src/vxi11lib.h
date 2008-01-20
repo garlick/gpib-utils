602af4991f36ddcabbb452f9e637b3fa9133ef0b
garlick <garlick@5738911d-5f10-0410-ae8c-d51ad623c20f>
2008-01-20 00:47:44 +0000

    more work

--- trunk/src/vxi11lib.h
+++ trunk/src/vxi11lib.h
@@ -20,6 +20,62 @@ typedef enum {
     VXI11_DOCMD_IFC_CONTROL   = 0x020010,
 } vxi11_docmd_cmd_t;
 
+#define VXI_MAX_READWRITE   (0xFFFFFFFF - 1)
+
+/* VXI-11 device_read 'reason' bits */
+#define VXI_REASON_REQCNT   0x01    /* requested size transferred */
+#define VXI_REASON_CHR      0x02    /* VXI_TERMCHRSET & termchar read */
+#define VXI_REASON_END      0x04    /* end indicator read */
+
+/* VXI-11 flags */
+#define VXI_WAITLOCK        0x01
+#define VXI_ENDW            0x08    /* only valid for device_write */
+#define VXI_TERMCHRSET      0x80    /* only valid for device_read */
+
+/* VXI-11 errors (specification 1.0) */
+#define VXI11_ERR_SUCCESS   0
+#define VXI11_ERR_SYNTAX    1
+#define VXI11_ERR_NODEVICE  3
+#define VXI11_ERR_LINKINVAL 4
+#define VXI11_ERR_PARAMETER 5
+#define VXI11_ERR_NOCHAN    6
+#define VXI11_ERR_NOTSUPP   8
+#define VXI11_ERR_RESOURCES 9
+#define VXI11_ERR_LOCKED    11
+#define VXI11_ERR_NOLOCK    12
+#define VXI11_ERR_IOTIMEOUT 15
+#define VXI11_ERR_IOERROR   17
+#define VXI11_ERR_ADDRINVAL 21
+#define VXI11_ERR_ABORT     23
+#define VXI11_ERR_CHANEST   29
+
+/* Internal errors */
+#define VXI11_ERR_RPCERR    100
+#define VXI11_ERR_ABRTINFO  101
+#define VXI11_ERR_TOOBIG    102
+
+#define VXI11_ERRORS { \
+    { VXI11_ERR_SUCCESS,    "no error" }, \
+    { VXI11_ERR_SYNTAX,     "syntax error" }, \
+    { VXI11_ERR_NODEVICE,   "device not accessible" }, \
+    { VXI11_ERR_LINKINVAL,  "invalid link identifier" }, \
+    { VXI11_ERR_PARAMETER,  "parameter error" }, \
+    { VXI11_ERR_NOCHAN,     "channel not established" }, \
+    { VXI11_ERR_NOTSUPP,    "operation not supported" }, \
+    { VXI11_ERR_RESOURCES,  "out of resources" }, \
+    { VXI11_ERR_LOCKED,     "device locked by another link" }, \
+    { VXI11_ERR_NOLOCK,     "no lock held by this link" }, \
+    { VXI11_ERR_IOTIMEOUT,  "I/O timeout" }, \
+    { VXI11_ERR_IOERROR,    "I/O error" }, \
+    { VXI11_ERR_ADDRINVAL,  "invalid address" }, \
+    { VXI11_ERR_ABORT,      "abort" }, \
+    { VXI11_ERR_CHANEST,    "channel already established" }, \
+    { VXI11_ERR_RPCERR,     "RPC error" }, \
+    { VXI11_ERR_ABRTINFO,   "getaddrinfo on abort port failed" }, \
+    { VXI11_ERR_TOOBIG,     "write buffer exceeds receive size" }, \
+    { 0, 0 }, \
+}
+
 /*
  * vi:tabstop=4 shiftwidth=4 expandtab
  */
